73ef7d13b1ee8309ea4b04bdc98963b828f254e1
Ivon Martinez <ivon.mrtnz@gmail.com>
2024-07-17 00:59:29 +0100

    script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById('mortgageForm').addEventListener('submit', calculateRepa
 const form = document.getElementById('mortgageForm');
 
 
+
 function calculateRepayments(event) {
     event.preventDefault();
 
@@ -9,22 +10,27 @@ function calculateRepayments(event) {
     const yearsMortgage = parseFloat(document.getElementById('yearsMortgage').value);
     const mortgageInterest = parseFloat(document.getElementById('mortgageInterest').value);
 
-    const mortgageType = document.querySelector('input[name = "mortgageType]:checked').value;
+    const mortgageType = document.querySelector('input[name = "mortgageType"]:checked').value;
 
     let monthlyRepayment, totalRepayment;
 
     if (mortgageType === 'mortgageRepayment') {
-        const x = Math.pow(1+mortgageInterest, yearsMortgage);
-        monthlyRepayment = (mortgageAmount * mortgageInterest * x) / (x - 1);
+        const n = yearsMortgage * 12;
+        const r = (mortgageInterest/100)/12;
+        const x = Math.pow(1+r, n);
+        monthlyRepayment = mortgageAmount * ((r * x) / (x - 1));
+        totalRepayment = monthlyRepayment * n;
     } 
     else {
-        monthlyRepayment = (mortgageAmount * mortgageInterest);
+        const n = yearsMortgage * 12;
+        const r = (mortgageInterest/100)/12;
+        monthlyRepayment = (mortgageAmount * r);
+        totalRepayment = monthlyRepayment * n;
     }
 
-    totalRepayment = monthlyRepayment * yearsMortgage;
 
-    document.getElementById('monthlyRepayment').textContent = `Your monthly repayments: $${monthlyRepayment.toFixed(2)}`;
-    document.getElementById('totalRepayment').textContent = `Total you'll repay over the term: $${totalRepayment.toFixed(2)}`;
+    document.getElementById('monthlyRepayment').textContent = `Your monthly repayments: £${monthlyRepayment.toFixed(2)}`;
+    document.getElementById('totalRepayment').textContent = `Total you'll repay over the term: £${totalRepayment.toFixed(2)}`;
 }
 
 function clearForm(){
